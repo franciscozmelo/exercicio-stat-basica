859678e9a585339a4c4d00998c2fe7233b5063df
Francisco Melo <chicomelo2112@gmail.com>
2026-03-25 00:43:32 -0300

    Atualização analise R

--- analise.R
+++ analise.R
@@ -2,3 +2,9 @@ Votos_Lula <- eleicoes %>%
 group_by(candidato == "Lula") %>%
 summarise(soma_Lula = sum(votos)) %>% head()
 print(Votos_Lula)
+
+top_lula <- eleicoes %>% 
+  filter(candidato == "Lula") %>% 
+  arrange(desc(votos)) %>% 
+  head(5)
+print(top_lula)
